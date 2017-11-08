caa388335ed2e81b6cf722d453be75273e2b5f7b
vosst <thomas@airmap.com>
2017-11-08 18:23:35 +0100

    Fine-tune versions

--- utils/airmapd.sh
+++ utils/airmapd.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-variant=${1}
+variant="$1"
 
-if [ "${variant}" -eq "linux" ]; then
+if [ "${variant}"="linux" ]; then
     CC=/usr/bin/gcc CXX=/usr/bin/g++ ./mason build   boost       1.63.0
     CC=/usr/bin/gcc CXX=/usr/bin/g++ ./mason publish boost       1.63.0
 
@@ -26,12 +26,15 @@ if [ "${variant}" -eq "linux" ]; then
     CC=/usr/bin/gcc CXX=/usr/bin/g++ ./mason build   boost_libtest            1.63.0
     CC=/usr/bin/gcc CXX=/usr/bin/g++ ./mason publish boost_libtest            1.63.0
 
-    CC=/usr/bin/gcc CXX=/usr/bin/g++ ./mason build   protobuf                 3.3.0
-    CC=/usr/bin/gcc CXX=/usr/bin/g++ ./mason publish protobuf            	  3.3.0
+    CC=/usr/bin/gcc CXX=/usr/bin/g++ ./mason build   openssl                  1.0.2
+    CC=/usr/bin/gcc CXX=/usr/bin/g++ ./mason publish openssl                  1.0.2
+
+    CC=/usr/bin/gcc CXX=/usr/bin/g++ ./mason build   protobuf                 3.0.0
+    CC=/usr/bin/gcc CXX=/usr/bin/g++ ./mason publish protobuf            	  3.0.0
 fi
 
 
-if [ "${variant}" -eq "android-arm-v7" ]; then
+if [ "${variant}"="android-arm-v7" ]; then
     MASON_PLATFORM=android MASON_ANDROID_ABI=arm-v7 ./mason build   boost_libdate_time       1.63.0
     MASON_PLATFORM=android MASON_ANDROID_ABI=arm-v7 ./mason publish boost_libdate_time       1.63.0
 
@@ -50,8 +53,11 @@ if [ "${variant}" -eq "android-arm-v7" ]; then
     MASON_PLATFORM=android MASON_ANDROID_ABI=arm-v7 ./mason build   boost_libtest            1.63.0
     MASON_PLATFORM=android MASON_ANDROID_ABI=arm-v7 ./mason publish boost_libtest            1.63.0
 
-    MASON_PLATFORM=android MASON_ANDROID_ABI=arm-v7 ./mason build   protobuf                 3.3.0
-    MASON_PLATFORM=android MASON_ANDROID_ABI=arm-v7 ./mason publish protobuf            	 3.3.0
+    MASON_PLATFORM=android MASON_ANDROID_ABI=arm-v7 ./mason build   openssl                  1.0.2
+    MASON_PLATFORM=android MASON_ANDROID_ABI=arm-v7 ./mason publish openssl                  1.0.2
+
+    MASON_PLATFORM=android MASON_ANDROID_ABI=arm-v7 ./mason build   protobuf                 3.0.0
+    MASON_PLATFORM=android MASON_ANDROID_ABI=arm-v7 ./mason publish protobuf            	 3.0.0
 fi
 
 
