8b9f675b79d564c6dbfcac82389abf5c435a8dd9
vosst <thomas@airmap.com>
2017-11-09 21:25:18 +0100

    Iterate on boost test setup and switch to openssl 1.0.1p

--- utils/airmapd.sh
+++ utils/airmapd.sh
@@ -7,8 +7,8 @@ variant="$1"
 if test "${variant}" = "linux"; then
     echo "building airmapd dependencies for linux x86-x64"
 
-    CC=/usr/bin/gcc CXX=/usr/bin/g++ ./mason build   boost       1.63.0
-    CC=/usr/bin/gcc CXX=/usr/bin/g++ ./mason publish boost       1.63.0
+    CC=/usr/bin/gcc CXX=/usr/bin/g++ ./mason build   boost                    1.63.0
+    CC=/usr/bin/gcc CXX=/usr/bin/g++ ./mason publish boost                    1.63.0
 
     CC=/usr/bin/gcc CXX=/usr/bin/g++ ./mason build   boost_libdate_time       1.63.0
     CC=/usr/bin/gcc CXX=/usr/bin/g++ ./mason publish boost_libdate_time       1.63.0
@@ -28,11 +28,11 @@ if test "${variant}" = "linux"; then
     CC=/usr/bin/gcc CXX=/usr/bin/g++ ./mason build   boost_libtest            1.63.0
     CC=/usr/bin/gcc CXX=/usr/bin/g++ ./mason publish boost_libtest            1.63.0
 
-    CC=/usr/bin/gcc CXX=/usr/bin/g++ ./mason build   openssl                  1.0.2
-    CC=/usr/bin/gcc CXX=/usr/bin/g++ ./mason publish openssl                  1.0.2
+    CC=/usr/bin/gcc CXX=/usr/bin/g++ ./mason build   openssl                  1.0.1p
+    CC=/usr/bin/gcc CXX=/usr/bin/g++ ./mason publish openssl                  1.0.1p
 
     CC=/usr/bin/gcc CXX=/usr/bin/g++ ./mason build   protobuf                 3.0.0
-    CC=/usr/bin/gcc CXX=/usr/bin/g++ ./mason publish protobuf            	  3.0.0
+    CC=/usr/bin/gcc CXX=/usr/bin/g++ ./mason publish protobuf                 3.0.0
 fi
 
 
@@ -57,11 +57,11 @@ if test "${variant}" = "android-arm-v7"; then
     MASON_PLATFORM=android MASON_ANDROID_ABI=arm-v7 ./mason build   boost_libtest            1.63.0
     MASON_PLATFORM=android MASON_ANDROID_ABI=arm-v7 ./mason publish boost_libtest            1.63.0
 
-    MASON_PLATFORM=android MASON_ANDROID_ABI=arm-v7 ./mason build   openssl                  1.0.2
-    MASON_PLATFORM=android MASON_ANDROID_ABI=arm-v7 ./mason publish openssl                  1.0.2
+    MASON_PLATFORM=android MASON_ANDROID_ABI=arm-v7 ./mason build   openssl                  1.0.1p
+    MASON_PLATFORM=android MASON_ANDROID_ABI=arm-v7 ./mason publish openssl                  1.0.1p
 
     MASON_PLATFORM=android MASON_ANDROID_ABI=arm-v7 ./mason build   protobuf                 3.0.0
-    MASON_PLATFORM=android MASON_ANDROID_ABI=arm-v7 ./mason publish protobuf            	 3.0.0
+    MASON_PLATFORM=android MASON_ANDROID_ABI=arm-v7 ./mason publish protobuf                 3.0.0
 fi
 
 if test "${variant}" = "osx"; then
@@ -85,8 +85,8 @@ if test "${variant}" = "osx"; then
     MASON_PLATFORM=osx ./mason build   boost_libtest            1.63.0
     MASON_PLATFORM=osx ./mason publish boost_libtest            1.63.0
 
-    MASON_PLATFORM=osx ./mason build   openssl                  1.0.2
-    MASON_PLATFORM=osx ./mason publish openssl                  1.0.2
+    MASON_PLATFORM=osx ./mason build   openssl                  1.0.1p
+    MASON_PLATFORM=osx ./mason publish openssl                  1.0.1p
 
     MASON_PLATFORM=osx ./mason build   protobuf                 3.0.0
     MASON_PLATFORM=osx ./mason publish protobuf            	    3.0.0
